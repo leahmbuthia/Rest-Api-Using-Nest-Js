b75afea5c325a4ca6eb1c41fad6180a613c36e5c
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-29 15:11:37 +0300

    adding user and connecting prisma folder to app module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './auth.service';
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Req } from "@nestjs/common";
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController{
@@ -7,7 +8,9 @@ export class AuthController{
     //    authService.test()
     }
     @Post('signup')
-    signup(){
+    signup(@Req() req: Request){
+        console.log(req.body);
+
      return this.authService.signup
     }
     
