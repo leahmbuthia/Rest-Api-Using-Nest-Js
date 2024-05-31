3e17fb91f98dbeaccf952f77e9b8e42292ce2bab
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-31 10:47:51 +0300

    Done with Sign Up and Throw exception

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService{
@@ -11,19 +12,35 @@ export class AuthService{
     // generate the passwprd hash
     const hash =await argon.hash(dto.password);
 
-    // save the user in the db
-    const user = await this.prisma.user.create({
-        data:{
-            email:dto.email,
-            hash,
-        },
-    });
-    // return the saved user,excluding the hash from the response
-    const { hash: _, ...result } = user;
-    return result;
+try {
+        // save the user in the db
+        const user = await this.prisma.user.create({
+            data:{
+                email:dto.email,
+                hash,
+            },
+        });
+        // return the saved user,excluding the hash from the response
+        const { hash: _, ...result } = user;
+        return result;
+    }
+ catch (error) {
+    if(error instanceof PrismaClientKnownRequestError){
+        if(error.code === 'P2002') {
+            throw new ForbiddenException(
+                'Credentials taken'
+            )
+        }
+    }
+    throw error;
 }
+ }
 
 signin(){
+    // find user by email
+    // if user does not exist throw exception
+    // compare password
+    // send back feedback 
     return {msg: "Hello signed in"};
 }
 }
