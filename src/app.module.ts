a48e2f9df3cb7e0b8497418db45bbfabf05d8276
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-31 12:18:07 +0300

    Adding nest Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from 'nest-config';
+
 
 
 @Module({
   imports: [
-    ConfigModule. forRoot({}),AuthModule, UserModule, BookmarkModule, PrismaModule],
+    ConfigModule. forRoot({IIsGlobal: true}),AuthModule, UserModule, BookmarkModule, PrismaModule],
 
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from 'nest-config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(){
+    constructor(config: ConfigService){
         super({
             datasources:{
                 db:{
-                    url: 'postgresql://postgres:123@localhost:5434/nest?schema=public'
+                    url: config.get('DATABASE_URL'),
                 },
             },
 
         });
+        // prisma.user.findMany();
     };
 }
