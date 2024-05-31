06a2e79316180842fcab24c95d1854d9bbe08d1d
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-31 09:32:52 +0300

    Introduction to pipe function and class-valiadator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 @Injectable({})
 export class AuthService{
-    constructor (private prisma: PrismaService){
+    constructor (private prisma: PrismaService){}
 
-    }
+ async signup(dto: AuthDto){
+    // generate the passwprd hash
+    const hash =await argon.hash(dto.password);
 
-signup(){
-    
-    return {msg: "Hello signed up"};
+    // save the user in the db
+    const user = await this.prisma.user.create({
+        data:{
+            email:dto.email,
+            hash,
+        },
+    });
+    // return the saved user,excluding the hash from the response
+    const { hash: _, ...result } = user;
+    return result;
 }
 
 signin(){
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+import { PrismaModule } from '../prisma/prisma.module'; // Adjust the import path as necessary
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [AuthService],
+  exports: [AuthService],
+})
 export class UserModule {}
+
